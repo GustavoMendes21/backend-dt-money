19ade1e8595d9bbf32aa386ae587adeb1ae3f52c
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-08-06 16:21:20 -0300

    feat: Add an invalid data check

--- src/modules/users/controllers/createUserController.ts
+++ src/modules/users/controllers/createUserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
 
 import { CreateUserUseCase } from "../useCases/CreateUserUseCase";
 
@@ -6,6 +7,12 @@ const createUserUseCase = new CreateUserUseCase();
 
 class CreateUserController {
   async handle(request: Request, response: Response) {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ InvalidFields: errors.array() });
+    }
+
     const { name, email, password } = request.body;
 
     try {
