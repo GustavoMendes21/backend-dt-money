f6ee814071f21b8b2b90b5c1865f4164a52cab17
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-08-06 16:17:52 -0300

    feat: Add a data validation middleware in the create user route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 import { CreateUserController } from "../modules/users/controllers/createUserController";
+import { CreateUserValidator } from "../validators/CreateUserValidator";
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
 
-usersRouter.post("/create", createUserController.handle);
+usersRouter.post("/create", CreateUserValidator, createUserController.handle);
 
 export { usersRouter };
