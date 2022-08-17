2c55b54709a106cf6e7b08997d42bd4998bf5614
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-08-17 15:19:36 -0300

    feat: Create route to authenticate user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "../modules/users/controllers/AuthenticateUserController";
 
 import { CreateUserController } from "../modules/users/controllers/createUserController";
 import { CreateUserValidator } from "../validators/CreateUserValidator";
@@ -6,7 +7,9 @@ import { CreateUserValidator } from "../validators/CreateUserValidator";
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController()
 
 usersRouter.post("/create", CreateUserValidator, createUserController.handle);
+usersRouter.post("/auth", authenticateUserController.handle)
 
 export { usersRouter };
