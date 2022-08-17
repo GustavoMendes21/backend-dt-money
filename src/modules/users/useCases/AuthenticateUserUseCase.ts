d11e88def093882a94cdd68380f4b81e6679ccef
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-08-17 18:34:01 -0300

    feat: Create User Authentication UseCase

---
+++ src/modules/users/useCases/AuthenticateUserUseCase.ts
@@ -0,0 +1,33 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import { UsersRepository } from "../repositories/implementations/UsersRepository";
+
+interface IAuthenticateUserDTO {
+  email: string;
+  password: string;
+}
+
+const userRepository = new UsersRepository();
+
+class AuthenticateUserUseCase {
+  async execute({ email, password }: IAuthenticateUserDTO) {
+    const user = await userRepository.findByEmail({ email });
+    if (!user) {
+      throw new Error("Email not found");
+    }
+    const passwordHashed = user.password;
+    const passwordIsCorrect = await bcrypt.compare(password, passwordHashed);
+
+    if (passwordIsCorrect) {
+      const token = jwt.sign({ user: user.email }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "2 days",
+      });
+
+      return token;
+    }
+    throw new Error("Password is not correct");
+  }
+}
+
+export { AuthenticateUserUseCase };
