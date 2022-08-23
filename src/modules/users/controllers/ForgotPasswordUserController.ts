42dd4255719ea9599750479d0e88b3d959ab6207
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-08-23 15:34:38 -0300

    feat: Add forgot password controller

---
+++ src/modules/users/controllers/ForgotPasswordUserController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+import { ForgotPasswordUserUseCase } from "../useCases/ForgotPasswordUserUseCase";
+
+const forgotPasswordUserUseCase = new ForgotPasswordUserUseCase();
+
+class ForgotPasswordUserController {
+  // eslint-disable-next-line consistent-return
+  async handle(request: Request, response: Response) {
+    const { email } = request.body;
+
+    try {
+      await forgotPasswordUserUseCase.execute({ email });
+      return response
+        .status(201)
+        .json({ message: "Token Atualizado com sucesso" });
+    } catch (error) {
+      response.status(404).json({ erro: error.message });
+    }
+  }
+}
+
+export { ForgotPasswordUserController };
