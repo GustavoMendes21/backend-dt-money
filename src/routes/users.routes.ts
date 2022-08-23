e9f95cbc0872665cc590176ba44e2e66dc19bebc
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-08-23 15:32:32 -0300

    feat: Add forgot password route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
-import { AuthenticateUserController } from "../modules/users/controllers/AuthenticateUserController";
 
+import { AuthenticateUserController } from "../modules/users/controllers/AuthenticateUserController";
 import { CreateUserController } from "../modules/users/controllers/createUserController";
+import { ForgotPasswordUserController } from "../modules/users/controllers/ForgotPasswordUserController";
 import { CreateUserValidator } from "../validators/CreateUserValidator";
 
 const usersRouter = Router();
 
 const createUserController = new CreateUserController();
-const authenticateUserController = new AuthenticateUserController()
+const authenticateUserController = new AuthenticateUserController();
+const forgotPasswordUserController = new ForgotPasswordUserController();
 
 usersRouter.post("/create", CreateUserValidator, createUserController.handle);
-usersRouter.post("/auth", authenticateUserController.handle)
+usersRouter.post("/auth", authenticateUserController.handle);
+usersRouter.post("/auth/forgotPassword", forgotPasswordUserController.handle);
 
 export { usersRouter };
