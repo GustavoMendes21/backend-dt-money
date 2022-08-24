2df31edcbf9b2fc2866e2f1ae1c118540a5f5415
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-08-23 21:49:15 -0300

    feat: Add route Recovery Password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "../modules/users/controllers/AuthenticateUserController";
 import { CreateUserController } from "../modules/users/controllers/createUserController";
 import { ForgotPasswordUserController } from "../modules/users/controllers/ForgotPasswordUserController";
+import { RecoveryPasswordUserController } from "../modules/users/controllers/RecoveryPasswordUserController";
 import { CreateUserValidator } from "../validators/CreateUserValidator";
 
 const usersRouter = Router();
@@ -10,9 +11,14 @@ const usersRouter = Router();
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 const forgotPasswordUserController = new ForgotPasswordUserController();
+const recoveryPasswordUserController = new RecoveryPasswordUserController();
 
 usersRouter.post("/create", CreateUserValidator, createUserController.handle);
 usersRouter.post("/auth", authenticateUserController.handle);
 usersRouter.post("/auth/forgotPassword", forgotPasswordUserController.handle);
+usersRouter.post(
+  "/auth/RecoveryPassword",
+  recoveryPasswordUserController.handle
+);
 
 export { usersRouter };
