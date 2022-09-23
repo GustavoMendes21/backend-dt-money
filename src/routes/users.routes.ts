4a614b682b671e5a7d411ac9db1b22839fe65819
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-09-22 22:55:56 -0300

    feat: add validators on all routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,7 +4,10 @@ import { AuthenticateUserController } from "../modules/users/controllers/Authent
 import { CreateUserController } from "../modules/users/controllers/CreateUserController";
 import { ForgotPasswordUserController } from "../modules/users/controllers/ForgotPasswordUserController";
 import { RecoveryPasswordUserController } from "../modules/users/controllers/RecoveryPasswordUserController";
+import { AuthenticateUserValidator } from "../validators/AuthenticateUserValidator";
 import { CreateUserValidator } from "../validators/CreateUserValidator";
+import { ForgotPasswordValidator } from "../validators/ForgotPasswordUserController";
+import { RecoveryPasswordValidator } from "../validators/RecoveryPasswordValidator";
 
 const usersRouter = Router();
 
@@ -14,10 +17,21 @@ const forgotPasswordUserController = new ForgotPasswordUserController();
 const recoveryPasswordUserController = new RecoveryPasswordUserController();
 
 usersRouter.post("/create", CreateUserValidator, createUserController.handle);
-usersRouter.post("/auth", authenticateUserController.handle);
-usersRouter.post("/auth/forgotPassword", forgotPasswordUserController.handle);
+usersRouter.post(
+  "/auth",
+  AuthenticateUserValidator,
+  authenticateUserController.handle
+);
+
+usersRouter.post(
+  "/auth/forgotPassword",
+  ForgotPasswordValidator,
+  forgotPasswordUserController.handle
+);
+
 usersRouter.post(
   "/auth/RecoveryPassword",
+  RecoveryPasswordValidator,
   recoveryPasswordUserController.handle
 );
 
