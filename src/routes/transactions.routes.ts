9854a5942f9fd985aac4ec930ed17091d535a808
Gustavo Mendes <gustavomendes2112@hotmail.com>
2022-09-24 00:09:25 -0300

    feat: add route list transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,11 +2,23 @@ import { Router } from "express";
 
 import { AuthUser } from "../middleware/AuthUser";
 import { CreateTransactionController } from "../modules/transactions/controllers/CreateTransactionController";
+import { ListTransactionsController } from "../modules/transactions/controllers/ListTransactionsController";
 
 const transactionsRouter = Router();
 
 const createTransactionController = new CreateTransactionController();
+const listTransactionController = new ListTransactionsController();
 
-transactionsRouter.get("/create", AuthUser, createTransactionController.handle);
+transactionsRouter.post(
+  "/create",
+  AuthUser,
+  createTransactionController.handle
+);
+
+transactionsRouter.post(
+  "/listByUserId",
+  AuthUser,
+  listTransactionController.handle
+);
 
 export { transactionsRouter };
